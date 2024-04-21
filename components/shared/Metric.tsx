aba1dfb8b28e4b2cac9be6b19085f98edb56c404
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-04-21 11:07:39 +0200

    feat: pulse card

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: string | number;
+  href?: string;
+  title: string;
+  textStyle?: string;
+  isAuthor?: boolean;
+}
+
+const Metric: FC<MetricProps> = ({ imgUrl, alt, value, href, textStyle }) => {
+  const metricContent = (
+    <div className="flex cursor-pointer gap-2 p-2 hover:rounded-full hover:bg-primary/30 hover:backdrop-blur-lg">
+      <Image
+        src={imgUrl}
+        alt={alt}
+        width={16}
+        height={16}
+        className={`object-contain invert  ${href ? "size-[28px] rounded-full" : ""}`}
+      />
+      <p className={`${textStyle} flex items-center gap-1`}>{value}</p>
+    </div>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className="flex items-center justify-center gap-1">
+        {metricContent}
+      </Link>
+    );
+  }
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-1">
+      {metricContent}
+    </div>
+  );
+};
+
+export default Metric;
