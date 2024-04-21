c5e68ea25091d01c519b7fd82f36c43144684f9c
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-04-21 11:03:07 +0200

    feat: create project page

---
+++ app/(root)/create-project/page.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import CreateRepo from "@/components/form/CreateRepo";
+
+interface pageProps {
+  searchParams: { message: string };
+}
+
+const Page: FC<pageProps> = ({ searchParams }) => {
+  return (
+    <>
+      <header className="flex py-4">
+        <h1 className="line-clamp-2 text-2xl font-bold sm:text-3xl">
+          Get Started: Bring Your Idea to Life
+        </h1>
+      </header>
+      <CreateRepo />
+    </>
+  );
+};
+
+export default Page;
