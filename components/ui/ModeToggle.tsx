c2cb4ec850204df19ea07f327873ab901a8219ff
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-04-21 11:01:40 +0200

    style (UI): adding shadcn components

---
+++ components/ui/ModeToggle.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          <SunIcon color="#A3A3A3" className="flex size-5 dark:hidden" />
+          <MoonIcon color="#737373" className="hidden size-5 dark:block" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ components/ui/animated-tooltip.tsx
@@ -0,0 +1,90 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import { motion, useTransform, useMotionValue, useSpring } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const AnimatedTooltip = ({
+  items,
+  className,
+}: {
+  items: {
+    id: string;
+    name: string;
+    designation: string;
+    image: string;
+  }[];
+  className?: string;
+}) => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const springConfig = { stiffness: 100, damping: 5 };
+  const x = useMotionValue(0);
+  const rotate = useSpring(
+    useTransform(x, [-100, 100], [-45, 45]),
+    springConfig
+  );
+  const translateX = useSpring(
+    useTransform(x, [-100, 100], [-50, 50]),
+    springConfig
+  );
+  const handleMouseMove = (event: any) => {
+    const halfWidth = event.target.offsetWidth / 2;
+    x.set(event.nativeEvent.offsetX - halfWidth);
+  };
+
+  return (
+    <>
+      {items.map((item, idx) => (
+        <div
+          className="group relative -mr-4"
+          key={item.name}
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          {hoveredIndex === idx && (
+            <motion.div
+              initial={{ opacity: 0, y: 20, scale: 0.6 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                scale: 1,
+                transition: {
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 10,
+                },
+              }}
+              exit={{ opacity: 0, y: 20, scale: 0.6 }}
+              style={{
+                translateX,
+                rotate,
+                whiteSpace: "nowrap",
+              }}
+              className="absolute -left-1/2 -top-16 z-50 flex translate-x-1/2 flex-col items-center justify-center rounded-md bg-popover px-3 py-2 text-xs shadow-xl"
+            >
+              <div className="absolute inset-x-10 -bottom-px z-30 h-px w-1/5 bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
+              <div className="absolute -bottom-px z-30 h-px w-2/5 bg-gradient-to-r from-transparent via-primary to-transparent " />
+              <div className="relative z-30 text-sm font-bold text-foreground">
+                {item.name}
+              </div>
+              <div className="text-xs text-muted-foreground">
+                {item.designation}
+              </div>
+            </motion.div>
+          )}
+          <Image
+            onMouseMove={handleMouseMove}
+            height={100}
+            width={100}
+            src={item.image}
+            alt={item.name}
+            className={cn(
+              `relative bg-white !m-0 size-14 rounded-full border border-primary object-cover object-top !p-0 transition duration-500  group-hover:z-30 group-hover:scale-105`,
+              className
+            )}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
