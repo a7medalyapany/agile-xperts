38804387a60294172d9b444bb6c9de0bfcf5ac13
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-05-20 11:47:43 +0300

    refactor: Update CreateRepo import to ProjectRepo in create-project/page.tsx

--- app/(root)/create-project/page.tsx
+++ app/(root)/create-project/page.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import CreateRepo from "@/components/form/CreateRepo";
+import CreateRepo from "@/components/form/ProjectRepo";
 
 interface pageProps {
   searchParams: { message: string };
