6c812f26e6541472ddf531b2a765a87eaf50e38a
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-05-24 07:50:43 +0300

    feat: Extract name from email in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function extractNameFromEmail(email: string) {
+  // Remove the domain part of the email
+  let username = email.split('@')[0];
+  
+  // Replace common separators with spaces
+  username = username.replace(/[._-]/g, ' ');
+  
+  // If the name is in "firstname.lastname" format, extract the first name
+  const name = username.split(' ')[0];
+  
+  return name;
+}
