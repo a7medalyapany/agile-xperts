b5c130bc4085a7770da9f53c753021fd81b68813
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-03 23:14:44 +0300

    feat: Add getImageData function to handle image upload and preview

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
+import React from "react";
 import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,4 +17,20 @@ export function extractNameFromEmail(email: string) {
   const name = username.split(' ')[0];
   
   return name;
+}
+
+
+export function getImageData(event: React.ChangeEvent<HTMLInputElement>) {
+  // FileList is immutable, so we need to create a new one
+  const dataTransfer = new DataTransfer();
+
+  // Add newly uploaded images
+  Array.from(event.target.files!).forEach((image) =>
+    dataTransfer.items.add(image)
+  );
+
+  const files = dataTransfer.files;
+  const displayUrl = URL.createObjectURL(event.target.files![0]);
+
+  return { files, displayUrl };
 }
