5f5bc71e85e9092558ec731dbbbe996a6c0fd541
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-11 04:59:21 +0300

    Fix: redirect /profile route to current profile page or login

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,35 @@
-import { type NextRequest } from "next/server";
+import { createClient } from "@/lib/supabase/server";
 import { updateSession } from "@/lib/supabase/middleware";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+
+  if (request.nextUrl.pathname === '/profile') {
+    const supabase = createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+    
+    if (error) {
+      console.error("Error fetching user:", error.message);
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (!user) {
+      console.error("User not authenticated");
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+    
+    const currentUserId = user.id;
+
+    return NextResponse.redirect(new URL(`/profile/${currentUserId}`, request.url));
+  }
+
+  return response;
 }
 
 export const config = {
   matcher: [
+    '/profile',
     // '/create-project',
     // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
