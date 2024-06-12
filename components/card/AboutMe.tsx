11d130f77da033a0b1df2111c6d857f70ef7d10f
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-12 05:37:12 +0300

    Refactor: AboutMe component to accept aboutMe as a prop and render dynamically

--- components/card/AboutMe.tsx
+++ components/card/AboutMe.tsx
@@ -2,11 +2,11 @@ import { FC } from "react";
 import ReadMore from "../shared/ReadMore";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-interface AboutMeProps {}
-const aboutMe =
-  "I'm a dedicated Computer Science student with a strong passion for developing innovative software solutions and a keen interest in mastering the latest tech trends. Currently based In Egypt, I bring a unique blend of cultural perspectives and technical expertise to tackle complex problems. My academic and personal projects reflect my commitment to learning and applying practical skills in real-world scenarios, from coding robust applications to diving deep into data structures and algorithms. Eager to contribute to and grow within a dynamic team, I aim to leverage my skills to become a super professional software engineer.";
+interface AboutMeProps {
+  aboutMe: string;
+}
 
-const AboutMe: FC<AboutMeProps> = () => {
+const AboutMe: FC<AboutMeProps> = ({ aboutMe }) => {
   return (
     <Card className="flex flex-col gap-4 pt-1.5 drop-shadow-md">
       <CardHeader className="py-0">
