97755bd7bf0781fa3deffab3b7b9bef7f0de1c5b
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-12 05:36:35 +0300

    Chore: Update MostUsedLanguage component to use Next.js Image component for better performance

--- components/card/MostUsedLanguage.tsx
+++ components/card/MostUsedLanguage.tsx
@@ -1,21 +1,31 @@
 import { FC } from "react";
+import Image from "next/image";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
-interface MostUsedLanguageProps {}
+interface MostUsedLanguageProps {
+  githubUsername?: string;
+}
 
-const MostUsedLanguage: FC<MostUsedLanguageProps> = () => {
-  const githubStatsImage =
-    "https://github-readme-stats.vercel.app/api/top-langs/?username=a7medalyapany&layout=compact&theme=transparent&hide_border=true&hide_title=true&langs_count=6";
+const MostUsedLanguage: FC<MostUsedLanguageProps> = ({ githubUsername }) => {
+  if (!githubUsername) return null;
+
+  const githubStatsImage = `https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUsername}&layout=compact&theme=transparent&hide_border=true&hide_title=true&langs_count=6`;
 
   return (
-    <Card className="flex flex-col pt-1.5 drop-shadow-md">
+    <Card className="flex grow flex-col pt-1.5 drop-shadow-md">
       <CardHeader className="py-0">
         <CardTitle className="text-balance leading-relaxed">
           Most Used Languages
         </CardTitle>
       </CardHeader>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={githubStatsImage} alt="Github stats" />
+      <Image
+        src={githubStatsImage}
+        width={400}
+        height={400}
+        unoptimized={true}
+        alt="Github stats"
+        className="size-fit"
+      />
     </Card>
   );
 };
