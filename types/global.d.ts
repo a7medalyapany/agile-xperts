8b2007ebfa55c5d2ea577141bc5a1071e0effd9c
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-13 06:34:48 +0300

    Refactor: Update global.d.ts to include socialMediaAccounts type

--- types/global.d.ts
+++ types/global.d.ts
@@ -5,8 +5,9 @@ import { Database as DB } from "@/types/database";
 type Pulse = DB["public"]["Tables"]["post"]["Row"];
 type Profile = DB["public"]["Tables"]["profile"]["Row"];
 type profileView = DB["public"]["Views"]["profile_view"]["Row"];
-type privateProfile = DB["public"]["Views"]["private_profile_view"]["Row"];
+type socialMediaAccounts = DB["public"]["Tables"]["social_media"]["Row"];
 type publicProfile = DB["public"]["Views"]["public_profile_view"]["Row"];
+type privateProfile = DB["public"]["Views"]["private_profile_view"]["Row"];
 
 declare global {
   type Database = DB;
