62b79a6bee0da88a02c6495701a6ffcec80de7af
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-13 04:52:45 +0300

    Refactor: Update middleware to handle security settings redirects

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
+import { cookies } from "next/headers";
 import { createClient } from "@/lib/supabase/server";
 import { updateSession } from "@/lib/supabase/middleware";
 import { NextResponse, type NextRequest } from "next/server";
-import { getUserRoleById } from "./lib/actions/user.action";
-import { cookies } from "next/headers";
+import { checkUserIdentity, getUserRoleById } from "./lib/actions/user.action";
 
 export async function middleware(request: NextRequest) {
   const response = await updateSession(request);
@@ -55,13 +55,40 @@ export async function middleware(request: NextRequest) {
     }
   }
 
+  if (request.nextUrl.pathname === '/settings/security') {
+    const supabase = createClient();
+    const { data: { user }, error } = await supabase.auth.getUser();
+    
+    if (error) {
+      console.error("Error fetching user:", error.message);
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (!user) {
+      console.error("User not authenticated");
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    const { identitiesNumber, hasGitHubIdentity } = await checkUserIdentity();
+
+    console.log(identitiesNumber, hasGitHubIdentity);
+
+    if (hasGitHubIdentity && identitiesNumber === 1) {
+      return NextResponse.redirect(new URL('/settings/security/github', request.url));
+    }
+    return NextResponse.next();
+  }
+
   return response;
 }
 
 export const config = {
   matcher: [
     '/profile',
+    '/settings', // make sure to add the settings pages *
     '/create-project',
+    '/settings/security',
+    '/settings/security/github',
     // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
