5c943e30ff1056475e8e47f1007c719cd20ddb5d
0xMuhammadEhab <muhammud.ehab7@gmail.com>
2024-06-13 11:09:22 -0400

    Feat: get recent projects in lobby

--- app/(root)/lobby/page.tsx
+++ app/(root)/lobby/page.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 import LobbyProject from "@/components/card/LobbyProject";
-import { techStack, projects } from "@/constants/dummy";
+import { techStack } from "@/constants/dummy";
+import { getLobby } from "@/lib/actions/lobby.action";
 
 interface pageProps {}
 
-const Page: FC<pageProps> = () => {
+const Page: FC<pageProps> = async () => {
+  const projects = await getLobby();
+  console.log(projects[0].technologies);
   return (
     <>
       <header className="flex py-4">
@@ -15,12 +18,15 @@ const Page: FC<pageProps> = () => {
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
           <LobbyProject
-            key={project.id}
-            title={project.title}
-            imageUrl={project.imageUrl!}
-            techStack={techStack}
-            knockCount={project.knockCount}
-            createdAt={project.createdAt}
+            key={project.project_id}
+            title={project.project_title!}
+            imageUrl={project.project_img_url!}
+            techStack={
+              techStack
+              // project.technologies as { name: string; designation: string }[]
+            }
+            knockCount={project.request_count!}
+            createdAt={project.project_created_at!}
           />
         ))}
       </div>
