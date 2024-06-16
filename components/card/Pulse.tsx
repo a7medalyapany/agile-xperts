288df617efe81464457fcc480f20f89305ddac73
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-16 10:16:36 +0300

    Refactor: Update Metric component to include onClick prop

--- components/card/Pulse.tsx
+++ components/card/Pulse.tsx
@@ -1,10 +1,15 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { IPulseProps } from "@/types";
 import Metric from "../shared/Metric";
 import ReadMore from "../shared/ReadMore";
+import {
+  handlePulseBookMark,
+  handlePulseLike,
+} from "@/lib/actions/pulse.action";
 
 interface PulseProps extends IPulseProps {
   isEchoBack?: boolean;
@@ -18,16 +23,52 @@ const Pulse: FC<PulseProps> = ({
   authorId,
   authorName,
   authorAvatar,
-  likeCount,
+  likeCount: initialLikeCount,
   replyCount,
-  repostCount,
+  repostCount: initialRepostCount,
   isEchoBack,
   echoBack,
+  hasLiked,
+  hasReposted,
+  hasBookmarked,
 }) => {
+  const [isBookmarked, setIsBookmarked] = useState(hasBookmarked);
+  const [likeCount, setLikeCount] = useState(initialLikeCount);
+  const [repostCount, setRepostCount] = useState(initialRepostCount);
+
+  const handleLikeClick = async () => {
+    const optimisticLikeCount = initialLikeCount + (hasLiked ? -1 : 1);
+    setLikeCount(optimisticLikeCount);
+
+    try {
+      const result = await handlePulseLike({ postId: id });
+      setLikeCount(result?.likeCount!);
+    } catch (error) {
+      console.error(error);
+      setLikeCount(initialLikeCount);
+    }
+  };
+
+  const handleRepostClick = () => {
+    const optimisticRepostCount = initialRepostCount + (hasReposted ? -1 : 1);
+    setRepostCount(optimisticRepostCount);
+
+    // Simulate a server request here for repost action
+    // Perform async action and update state if necessary
+  };
+
+  const handleBookmarkClick = async () => {
+    setIsBookmarked(!isBookmarked);
+    try {
+      await handlePulseBookMark({ postId: id });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <article
-      className={`flex w-full flex-col border-b p-0
-      sm:p-6  ${isEchoBack ? "py-6" : "pb-6"}`}
+      className={`flex w-full flex-col border-b p-0 sm:p-6 ${isEchoBack ? "py-6" : "pb-6"}`}
     >
       <div className="flex items-start">
         <div className="flex w-full flex-1 flex-row gap-2">
@@ -38,7 +79,6 @@ const Pulse: FC<PulseProps> = ({
                 <AvatarFallback>{authorName.charAt(0)}</AvatarFallback>
               </Avatar>
             </Link>
-
             <div className="relative mt-2 w-0.5 grow rounded-full bg-neutral-800" />
           </div>
 
@@ -48,40 +88,49 @@ const Pulse: FC<PulseProps> = ({
                 {authorName}
               </h4>
             </Link>
-
             <Link href={`/dev-pulse/${id}`} className="w-full">
               {content && (
                 <ReadMore text={content} className="text-start text-sm" />
               )}
-
               {photo && (
                 <Image
                   src={photo}
                   alt="pulse-photo"
                   width={1200}
                   height={400}
-                  className=" xs:h-[400px] h-64 w-full rounded-lg object-cover lg:h-[450px]"
+                  className="xs:h-[400px] h-64 w-full rounded-lg object-cover lg:h-[450px]"
                 />
               )}
             </Link>
-
             <div
               className={`${isEchoBack && "mb-10"} mt-5 flex flex-col gap-3`}
             >
-              <div className="flex w-full items-center justify-between">
+              <div
+                className={`${isEchoBack ? "hidden" : "flex w-full items-center justify-between"}`}
+              >
                 <Metric
-                  imgUrl="/assets/icons/echo.svg"
+                  imgUrl={
+                    hasLiked
+                      ? "/assets/icons/echo-filled.svg"
+                      : "/assets/icons/echo.svg"
+                  }
                   alt="Echo"
                   value={likeCount}
                   title="Echoes"
                   textStyle="small-medium text-muted-foreground"
+                  onClick={handleLikeClick}
                 />
                 <Metric
-                  imgUrl="/assets/icons/echoOut.svg"
-                  alt="Echo Out "
+                  imgUrl={
+                    hasReposted
+                      ? "/assets/icons/echoOut-filled.svg"
+                      : "/assets/icons/echoOut.svg"
+                  }
+                  alt="Echo Out"
                   value={repostCount}
                   title="Echo Outs"
                   textStyle="small-medium text-muted-foreground"
+                  onClick={handleRepostClick}
                 />
                 <Metric
                   imgUrl="/assets/icons/echoBack.svg"
@@ -90,21 +139,25 @@ const Pulse: FC<PulseProps> = ({
                   title="Replies"
                   textStyle="small-medium text-muted-foreground"
                 />
-                <Metric
+                {/* <Metric
                   imgUrl="/assets/icons/analytics.svg"
                   alt="Views"
                   value={228}
-                  title={`Views`}
+                  title="Views"
                   textStyle="small-medium text-muted-foreground"
-                />
+                /> */}
                 <Metric
-                  imgUrl={"/assets/icons/bookmarks-fill.svg"}
+                  imgUrl={
+                    isBookmarked
+                      ? "/assets/icons/bookmarks-fill.svg"
+                      : "/assets/icons/bookmarks.svg"
+                  }
                   alt="Bookmarks"
-                  title={`Bookmarks`}
+                  title="Bookmarks"
                   textStyle="small-medium text-muted-foreground"
+                  onClick={handleBookmarkClick}
                 />
               </div>
-
               {isEchoBack && echoBack.length > 0 && (
                 <Link href={`/dev-pulse/${id}`}>
                   <p className="mt-1 text-start text-base text-muted-foreground">
@@ -129,7 +182,6 @@ const Pulse: FC<PulseProps> = ({
               className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
             />
           ))}
-
           <Link href={`/dev-pulse/${id}`}>
             <p className="mt-1 text-base text-muted-foreground">
               {echoBack.length} repl{echoBack.length > 1 ? "ies" : "y"}
--- components/shared/Metric.tsx
+++ components/shared/Metric.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { formatNumber } from "@/lib/utils";
 
 interface MetricProps {
@@ -11,17 +11,34 @@ interface MetricProps {
   title: string;
   textStyle?: string;
   isAuthor?: boolean;
+  onClick?: () => void;
 }
 
-const Metric: FC<MetricProps> = ({ imgUrl, alt, value, href, textStyle }) => {
+const Metric: FC<MetricProps> = ({
+  imgUrl,
+  alt,
+  value,
+  href,
+  textStyle,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   const metricContent = (
-    <div className="flex cursor-pointer gap-2 p-2 hover:rounded-full hover:bg-primary/30 hover:backdrop-blur-lg">
+    <div
+      onClick={handleClick}
+      className="flex cursor-pointer gap-2 p-2 hover:rounded-full hover:bg-primary/30 hover:backdrop-blur-lg"
+    >
       <Image
         src={imgUrl}
         alt={alt}
         width={16}
         height={16}
-        className={`object-contain invert  ${href ? "size-[28px] rounded-full" : ""}`}
+        className={`object-contain invert ${href ? "size-[28px] rounded-full" : ""}`}
       />
       {value !== undefined && value > 0 && (
         <p className={`${textStyle} flex items-center gap-1`}>
