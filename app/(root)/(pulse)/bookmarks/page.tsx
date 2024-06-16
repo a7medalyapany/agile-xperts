c981155be0078f0a20d75de02cb37aae88bf623f
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-16 10:17:14 +0300

    Refactor: Update bookmarks page to display user bookmarks with BookmarksPulse component

--- app/(root)/(pulse)/bookmarks/page.tsx
+++ app/(root)/(pulse)/bookmarks/page.tsx
@@ -1,22 +1,44 @@
 import { FC } from "react";
-import Pulse from "@/components/card/Pulse";
-
-import { pulses } from "@/constants/dummy";
+import Link from "next/link";
+import Image from "next/image";
+import { getUserBookmarks } from "@/lib/actions/pulse.action";
+import BookmarksPulse from "@/components/card/BookedMarkedPulse";
 
 interface pageProps {}
 
-const Page: FC<pageProps> = () => {
+const Page: FC<pageProps> = async () => {
+  const pulses = await getUserBookmarks();
+  console.log(pulses);
+
   return (
     <div className="size-full space-y-4 overflow-auto sm:rounded-lg sm:border">
+      {pulses.length === 0 && (
+        <div className="my-20 flex h-full flex-col items-center justify-center gap-6 drop-shadow-lg">
+          <Image
+            src="/assets/images/agile-xperts.png"
+            alt="empty"
+            width={200}
+            height={200}
+            className="size-fit rounded-lg"
+          />
+          <Link
+            className="rounded-lg bg-foreground px-4 py-2 text-background"
+            href={"/dev-pulse"}
+          >
+            Go save some!
+          </Link>
+          <h4>No bookmarks found</h4>
+        </div>
+      )}
       {pulses.map((pulse) => (
-        <Pulse
+        <BookmarksPulse
           key={pulse.id}
           id={pulse.id}
-          author={pulse.author}
-          content={pulse.content}
-          photo={pulse.photo}
-          isEchoBack={pulse.isEchoBack}
-          echoBack={pulse.echoBack}
+          authorId={pulse.profile?.id!}
+          authorName={pulse.profile?.name!}
+          authorAvatar={pulse.profile?.avatar_url!}
+          photo={pulse.post?.img_url}
+          content={pulse.post?.content}
         />
       ))}
     </div>
