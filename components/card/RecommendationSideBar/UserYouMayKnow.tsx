1b682b33eafa06c02b4d89527f46f2a875fd0834
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-06-20 13:30:06 +0300

    Refactor: UserYouMayKnow component and update UI

--- components/card/RecommendationSideBar/UserYouMayKnow.tsx
+++ components/card/RecommendationSideBar/UserYouMayKnow.tsx
@@ -1,33 +1,35 @@
-import { AvatarImage, Avatar } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
-
-
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { AvatarImage, Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 export function UserYouMayKnow() {
   return (
-    <Card className="w-full max-w-md ">
-    <CardHeader>
-      <CardTitle className="text-xl">People You May Know</CardTitle>
-    </CardHeader>
-    <CardContent className="space-y-4">
-      {[1,2,3].map((value)=> {
-        return(
-        <div className="flex items-center justify-between gap-4" key={value}>
-          <Avatar>
-            <AvatarImage alt="Dummy1" src="/placeholder.svg" />
-          </Avatar>
-          <div className="flex-1">
-            <span className="text-foreground font-medium">Dummy1</span>
-            <span className="text-muted-foreground block">@dUMMY1</span>
-          </div>
-          <Button>Follow</Button>
-        </div>
+    <div className="rounded-t-lg border">
+      <h2 className="my-6 text-xl">People You May Know</h2>
+      {[1, 2, 3, 4, 5].map((value) => {
+        return (
+          <>
+            <Link
+              key={value}
+              href={`/profile/${"#"}`}
+              className="flex items-center justify-between p-4 hover:bg-muted "
+            >
+              <div className="flex gap-1">
+                <Avatar className="shrink-0 drop-shadow-lg">
+                  <AvatarImage src={"#"} />
+                  <AvatarFallback>cn</AvatarFallback>
+                </Avatar>
+                <div className="text-start">
+                  <p className="font-bold">Ahmed Ibrahim</p>
+                  <p className="text-sm text-muted-foreground">@username</p>
+                </div>
+              </div>
+              <button>follow</button>
+            </Link>
+            <Separator />
+          </>
         );
-       })}
-        
-    </CardContent>
-    </Card>
-    
-  )
+      })}
+    </div>
+  );
 }
