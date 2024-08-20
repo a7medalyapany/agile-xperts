b4a6dded9f2693b9b8856d48a60675d33c71e7bb
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-08-20 13:34:00 +0300

    Feat: Implement lazy loading for project images in ProjectResults component

--- components/shared/Search/Results/ProjectResults.tsx
+++ components/shared/Search/Results/ProjectResults.tsx
@@ -1,25 +1,45 @@
+import { FC, useEffect, useState } from "react";
 import LobbyProject from "@/components/card/LobbyProject";
-import { FC } from "react";
+import { useSearchParams } from "next/navigation";
+import { searchProjects } from "@/lib/actions/search.action";
 
-interface ProjectResultsProps {}
+interface ProjectResultsProps {
+  query: string;
+}
 
-const tachStack = [
-  { id: 31, name: "TypeScript", designation: "backend" },
-  { id: 32, name: "Nest.js", designation: "backend" },
-  { id: 33, name: "React", designation: "frontend" },
-];
-const ProjectResults: FC<ProjectResultsProps> = () => {
+const ProjectResults: FC<ProjectResultsProps> = ({ query }) => {
+  const [projects, setProjects] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
+  // Extract filters from searchParams, excluding "q" and "type" and flatten the values into a single array
+  const filters = Array.from(searchParams.entries())
+    .filter(([key]) => !["q", "type"].includes(key))
+    .flatMap(([, value]) => value.split("-"));
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      setLoading(true);
+      // Pass the query and the flattened filters array to searchProjects
+      const result = await searchProjects(query, filters);
+      setProjects(result);
+      setLoading(false);
+    };
+
+    fetchProjects();
+  }, [query]);
+
+  if (loading) return <div>Loading...</div>;
   return (
     <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-      {[1, 2, 3, 4].map((result, index) => (
+      {projects?.map((project: any) => (
         <LobbyProject
-          key={index}
-          projectId={10}
-          imageUrl={""}
-          title={""}
-          techStack={tachStack}
-          knockCount={0}
-          createdAt={"2024-06-16 11:19:01.612842+00"}
+          key={project.id!}
+          projectId={project.id!}
+          imageUrl={project.project_image_url}
+          title={project.project_title}
+          techStack={project.technologies}
+          knockCount={project.request_count}
+          createdAt={project.project_created_at}
         />
       ))}
     </div>
