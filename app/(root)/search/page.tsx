fc2526fad7ec975cb98e54722713dc8a710b38f4
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-09-18 07:31:26 +0300

    Refactor: Move SearchFilter component to SearchPage and remove SearchLayout

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Search from "@/components/shared/Search/SearchInput";
+import SearchFilter from "@/components/shared/Search/FilterSidebar";
 
 const SearchPage: React.FC = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-4">
-      <Search />
+    <div className="flex flex-col sm:space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <aside className="hidden w-fit sm:block sm:w-1/5">
+        <SearchFilter />
+      </aside>
+      <div className="block lg:max-w-2xl lg:flex-1">
+        <div className="flex min-h-screen flex-col items-center gap-4">
+          <Search />
+        </div>
+      </div>
     </div>
   );
 };
