ec6f0259718c0ca36b67230490658bffae871b9f
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-10-10 04:35:47 +0300

    Add userId as a prop to pass it down

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { createClient } from "@/lib/supabase/server";
 import Search from "@/components/shared/Search/SearchInput";
 import SearchFilter from "@/components/shared/Search/FilterSidebar";
 
-const SearchPage: React.FC = () => {
+const SearchPage: React.FC = async () => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <div className="flex flex-col sm:space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
       <aside className="hidden w-fit sm:block sm:w-1/5">
@@ -10,7 +16,7 @@ const SearchPage: React.FC = () => {
       </aside>
       <div className="block lg:max-w-2xl lg:flex-1">
         <div className="flex min-h-screen flex-col items-center gap-4">
-          <Search />
+          <Search userId={user?.id ?? null} />
         </div>
       </div>
     </div>
