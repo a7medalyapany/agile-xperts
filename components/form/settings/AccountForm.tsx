c3140fc11b1512e388c143769b1b0fb173ead067
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-10-15 05:02:18 +0300

    Feat: Enhance AccountForm with user data handling and social media link management

--- components/form/settings/AccountForm.tsx
+++ components/form/settings/AccountForm.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-import { z } from "zod";
-import { useFieldArray, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Icons } from "@/components/svg-icons/icons";
+import { Loader2, PlusIcon, TrashIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils";
+import { z } from "zod";
+import { accountFormSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import TagInput from "@/components/ui/TagInput";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { platformIcons } from "@/components/card/SocialAccounts";
 import {
   Form,
   FormControl,
@@ -14,26 +24,39 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import TagInput from "@/components/ui/TagInput";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { accountFormSchema } from "@/lib/validation";
+import { updateAccountForm } from "@/lib/actions/user.action";
 
+platformIcons.GitHub = <Icons.gitHub className="size-5" />;
 type AccountFormValues = z.infer<typeof accountFormSchema>;
 
-// This can come from database or API.
-const defaultValues: Partial<AccountFormValues> = {
-  aboutMe:
-    "Hey there! I'm [Your Name], a [Your Title/Role] with a passion for [Your Passion/Specialization]. With [X] years of experience in [Your Field], I've honed my skills in [Your Skills/Expertise]. I thrive in [Describe Your Work Environment/Preferred Projects], and I'm always down to tackle new challenges and learn something fresh. Let's connect and make magic happen!",
-  skills: [],
-  urls: [{ value: "http://twitter.com/ahmedalyapany1" }],
-};
+interface AccountFormProps {
+  userData: {
+    profile: {
+      id: string;
+      about_me: string | null;
+      skills: string[] | null;
+    };
+    socialAccounts: {
+      id: number;
+      platform: Database["public"]["Enums"]["social_media_platform"];
+      account_link: string;
+    }[];
+  };
+}
 
-export function AccountForm() {
+export default function AccountForm({ userData }: AccountFormProps) {
+  const [isPending, setIsPending] = useState(false);
+
   const form = useForm<AccountFormValues>({
     resolver: zodResolver(accountFormSchema),
-    defaultValues,
+    defaultValues: {
+      aboutMe: userData.profile.about_me || "",
+      skills: userData.profile.skills || [],
+      urls: userData.socialAccounts.map((account) => ({
+        platform: account.platform,
+        value: account.account_link,
+      })) || [{ platform: "Other", value: "" }],
+    },
     mode: "onChange",
   });
 
@@ -42,21 +65,57 @@ export function AccountForm() {
     name: "urls",
   });
 
-  function onSubmit(data: AccountFormValues) {
-    console.log(data);
-    // toast({
-    //   title: "You submitted the following values:",
-    //   description: (
-    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-    //     </pre>
-    //   ),
-    // })
+  async function onSubmit(data: AccountFormValues) {
+    setIsPending(true);
+    try {
+      const result = await updateAccountForm(data);
+      if (result.success) {
+        toast.success("Your profile has been successfully updated.");
+      }
+    } catch (error) {
+      toast.error("Failed to update profile. Please try again.");
+    } finally {
+      setIsPending(false);
+    }
   }
+
+  const detectPlatform = (url: string) => {
+    const platformPatterns = {
+      Google: /google\.com/,
+      LinkedIn: /linkedin\.com/,
+      X: /twitter\.com|x\.com/,
+      Facebook: /facebook\.com/,
+      Instagram: /instagram\.com/,
+      GitHub: /github\.com/,
+    };
+
+    for (const [platform, pattern] of Object.entries(platformPatterns)) {
+      if (pattern.test(url)) {
+        return platform as keyof typeof platformIcons;
+      }
+    }
+    return "Other";
+  };
+
+  const isFormChanged = () => {
+    const currentValues = form.getValues();
+    return (
+      currentValues.aboutMe !== userData.profile.about_me ||
+      JSON.stringify(currentValues.skills) !==
+        JSON.stringify(userData.profile.skills) ||
+      JSON.stringify(currentValues.urls) !==
+        JSON.stringify(
+          userData.socialAccounts.map((account) => ({
+            platform: account.platform,
+            value: account.account_link,
+          }))
+        )
+    );
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
           name="aboutMe"
@@ -66,7 +125,8 @@ export function AccountForm() {
               <FormControl>
                 <Textarea
                   rows={5}
-                  className="resize-none bg-muted text-foreground"
+                  className="resize-none bg-muted drop-shadow-lg"
+                  placeholder="Tell us about yourself..."
                   {...field}
                 />
               </FormControl>
@@ -85,47 +145,65 @@ export function AccountForm() {
               <FormLabel>Skills</FormLabel>
               <FormControl>
                 <TagInput
-                  placeholder="Please list your relevant skills here."
+                  placeholder="Add your skills..."
                   onKeywordsChange={(keywords: string[]) =>
                     field.onChange(keywords)
                   }
+                  initialKeywords={field.value}
                 />
               </FormControl>
               <FormDescription>
-                Paste multiple skills separated by commas, new lines, or tabs.
-                Remove Skills by pressing Backspace.
+                Add multiple skills separated by commas, new lines, or tabs.
+                Remove skills by pressing Backspace.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        <div className="flex flex-col">
+        <div className="space-y-4">
+          <FormLabel>Social Media Links</FormLabel>
           {fields.map((field, index) => (
             <FormField
               key={field.id}
               control={form.control}
-              name={`urls.${index}.value`}
-              render={({ field }) => (
+              name={`urls.${index}`}
+              render={({ field: urlField }) => (
                 <FormItem>
-                  <FormLabel className={cn(index !== 0 && "sr-only")}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && "sr-only")}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
                   <FormControl>
-                    <div className="flex flex-col gap-2 sm:flex-row">
-                      <Input {...field} />
-                      {fields.length > 0 && (
-                        <Button
-                          type="button"
-                          variant="destructive"
-                          onClick={() => remove(index)}
-                        >
-                          Remove URL
-                        </Button>
-                      )}
+                    <div className="flex items-center space-x-2">
+                      <div className="flex w-10 justify-center">
+                        {platformIcons[urlField.value.platform]}
+                      </div>
+                      <Input
+                        value={urlField.value.value}
+                        onChange={(e) =>
+                          urlField.onChange({
+                            ...urlField.value,
+                            value: e.target.value,
+                          })
+                        }
+                        placeholder={`Enter your ${urlField.value.platform} URL`}
+                        className="flex-1"
+                        onBlur={(e) => {
+                          const detectedPlatform = detectPlatform(
+                            e.target.value
+                          );
+                          urlField.onChange({
+                            ...urlField.value,
+                            platform: detectedPlatform,
+                          });
+                        }}
+                      />
+                      <Button
+                        type="button"
+                        variant="outline"
+                        size="icon"
+                        onClick={() => remove(index)}
+                      >
+                        <TrashIcon className="size-4" />
+                        <span className="sr-only">Remove URL</span>
+                      </Button>
                     </div>
                   </FormControl>
                   <FormMessage />
@@ -137,15 +215,29 @@ export function AccountForm() {
             <Button
               type="button"
               variant="outline"
-              className="mt-4 sm:w-[135px]"
-              onClick={() => append({ value: "" })}
+              className="w-full"
+              onClick={() => append({ platform: "Other", value: "" })}
             >
-              Add URL
+              <PlusIcon className="mr-2 size-4" />
+              Add Social Media Link
             </Button>
           )}
         </div>
 
-        <Button type="submit">Update account</Button>
+        <Button
+          type="submit"
+          className="w-full font-bold sm:w-[200px]"
+          disabled={!isFormChanged() || isPending}
+        >
+          {isPending ? (
+            <>
+              <Loader2 className="mr-2 size-4 animate-spin" />
+              Updating...
+            </>
+          ) : (
+            "Update Account"
+          )}
+        </Button>
       </form>
     </Form>
   );
