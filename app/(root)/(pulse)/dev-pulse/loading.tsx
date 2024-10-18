7a87e676fd576e6bba0ba9be2d2d04713638771f
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-10-18 15:12:22 +0300

    Feat: Add Skeleton for dev-pulse pages

---
+++ app/(root)/(pulse)/dev-pulse/loading.tsx
@@ -0,0 +1,34 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen w-full">
+      <div className="flex gap-6">
+        {/* Main Content */}
+        <div className="w-full space-y-6">
+          {/* Post Input Box */}
+          <div className="space-y-4">
+            <Skeleton className="h-28 w-full rounded-md" />
+            {/* <Skeleton className="h-10 w-24 rounded-md" /> */}
+          </div>
+
+          {/* Feed */}
+          {[...Array(10)].map((_, index) => (
+            <div key={index} className="space-y-4 rounded-md border p-4">
+              <div className="flex items-center gap-4">
+                <Skeleton className="size-12 rounded-full" />
+                <Skeleton className="h-6 w-32 rounded-md" />
+              </div>
+              <Skeleton className="h-6 w-full rounded-md" />
+              <div className="flex gap-4">
+                <Skeleton className="size-6 rounded-full" />
+                <Skeleton className="size-6 rounded-full" />
+                <Skeleton className="size-6 rounded-full" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
